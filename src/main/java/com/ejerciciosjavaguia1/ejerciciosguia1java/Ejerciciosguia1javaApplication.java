dd8f815816dd2d4571febbf2b7602ba6492dac62
jprivas30 <juanpr2512@gmail.com>
2023-02-16 10:22:06 -0300

    Avance ejercicios

--- src/main/java/com/ejerciciosjavaguia1/ejerciciosguia1java/Ejerciciosguia1javaApplication.java
+++ src/main/java/com/ejerciciosjavaguia1/ejerciciosguia1java/Ejerciciosguia1javaApplication.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.fasterxml.jackson.databind.JsonSerializable.Base;
 
 @SpringBootApplication
 public class Ejerciciosguia1javaApplication {
@@ -60,113 +59,172 @@ public static void promedioNotas(float nota1, float nota2, float nota3, float no
 //----------------------------Ejercicio 6 -------------------------------------
 //---------------------------Funcion Ejercicio 6 ------------------------------
 
+public static void sueldoSemanal(int valorHora, int cantidadHoras){
+	int sueldo= valorHora * cantidadHoras;
+	System.out.println("El valor a cobrar a la semana por trabajar "+ cantidadHoras + "horas es igual a : $" + sueldo);
+}
+
+//----------------------------Ejercicio 7 -------------------------------------
+//---------------------------Funcion Ejercicio 7 ------------------------------
+
+public static void valorPintor (int mts, int costo){
+    int presupuesto = mts * costo;
+    System.out.println("El valor por pintar "+ mts + " m2 es de $"+ presupuesto);
+}
+
+//----------------------------Ejercicio 8 -------------------------------------
+//---------------------------Funcion Ejercicio 8 ------------------------------
+
+public static void valorLLamada(float min, float seg, float valor){
+	float minutos= min;
+	float segundos= seg/60;
+	float tiempo= (minutos+segundos);
+	float costo=tiempo*valor; 
+	System.out.print("La duracion de la llamada es: "+ minutos + " min " + seg + " seg ");
+	System.out.print("El costo de la llamada es: $"+ costo);
+	System.out.print("El valor del segundo es: $"+ valor/(tiempo*60));
+
+}
+
+//----------------------------Ejercicio 9 -------------------------------------
+//---------------------------Funcion Ejercicio 9 ------------------------------
 
 	public static void main(String[] args){
+
 
 
 //----------------------------Ejercicio 1 -------------------------------------
 //Escriba un programa que solicite a un usuario dos números por teclado y calcule su suma, resta, producto y división
 
-Scanner dato1 = new Scanner(System.in);
+/*Scanner datos = new Scanner(System.in);
+
 System.out.println("Ingrese primer numero en la consola");
-int primerNumero= dato1.nextInt();
-
-Scanner dato2= new Scanner(System.in);
+int primerNumero= datos.nextInt();
 System.out.println("Ingrese segundo numero en la consola");
-int  segundoNumero= dato2.nextInt();
+int  segundoNumero= datos.nextInt();
 
-dato1.close();
-dato2.close();
+datos.close();
 
-operaciones(primerNumero, segundoNumero);
+operaciones(primerNumero, segundoNumero);*/
+
 
 //----------------------------Ejercicio 2 -------------------------------------
 //Escriba un programa que permita calcular el área de un rectángulo. ¿Cómo modificaría su programa para que ahora calcule el área de un cuadrado de igual medida al ancho del rectángulo anterior?
 
-Scanner base = new Scanner(System.in);
+/*Scanner medidas = new Scanner(System.in);
 System.out.println("Ingrese el valor de la base del rectangulo");
-int base1= base.nextInt();
+int base1= medidas.nextInt();
 
-Scanner altura= new Scanner(System.in);
 System.out.println("Ingrese el valor de la altura del rectangulo");
-int  altura1 = altura.nextInt();
+int  altura1 = medidas.nextInt();
 
 areaFiguras(base1, altura1);
 
-Scanner lado = new Scanner(System.in);
 System.out.println("Ingrese el valor del lado del cuadrado");
-int lado3= lado.nextInt();
+int lado3= medidas.nextInt();
 
-base.close();
-altura.close();
-lado.close();
+medidas.close();
 
-areaFiguras(lado3, lado3);
+areaFiguras(lado3, lado3);*/
+
 
 //----------------------------Ejercicio 3 -------------------------------------
 //Escriba un programa que, dada una cantidad de dinero en pesos chilenos ingresada por teclado, calcule la cantidad de dólares que se pueden comprar
 
-Scanner pesos = new Scanner(System.in);
+/*Scanner montos = new Scanner(System.in);
+
 System.out.println("Ingrese cuantos pesos posee: ");
-int pesosChilenos= pesos.nextInt();
+int pesosChilenos=montos.nextInt();
 
-Scanner cambio = new Scanner(System.in);
 System.out.println("Ingrese el valor dolar");
-int valorCambio= cambio.nextInt();
+int valorCambio= montos.nextInt();
 
-pesos.close();
-cambio.close();
+montos.close();
 
-cambioPesos(pesosChilenos,valorCambio);
+cambioPesos(pesosChilenos,valorCambio);*/
+
 
 //----------------------------Ejercicio 4 -------------------------------------
 //Escriba un algoritmo que permita calcular la edad de una persona según su año de nacimiento
 
-Scanner aNacimiento = new Scanner(System.in);
+/*Scanner aNios = new Scanner(System.in);
+
 System.out.println("Ingrese su año de nacimiento: ");
-int anioNacimiento= aNacimiento.nextInt();
+int anioNacimiento= aNios.nextInt();
 
-Scanner aActual = new Scanner(System.in);
 System.out.println("Ingrese el año actual");
-int anioActual= aActual.nextInt();
+int anioActual= aNios.nextInt();
 
-aNacimiento.close();
-aActual.close();
+aNios.close();
 
+edad(anioActual,anioNacimiento);*/
 
-edad(anioActual,anioNacimiento);
 
 //----------------------------Ejercicio 5 -------------------------------------
 //Escriba un programa que solicite al usuario 4 notas y calcule su promedio.
 
-Scanner primeraNota = new Scanner(System.in);
-System.out.println("Ingrese nota 1: ");
-float not1= primeraNota.nextFloat();
+/*Scanner notas = new Scanner(System.in);
 
-Scanner segundaNota = new Scanner(System.in);
-System.out.println("Ingrese nota 2: ");
-float not2= segundaNota.nextFloat();
+System.out.print("Ingrese nota 1: ");
+float not1= notas.nextFloat();
 
-Scanner terceraNota = new Scanner(System.in);
-System.out.println("Ingrese nota 3: ");
-float not3= terceraNota.nextFloat();
+System.out.print("Ingrese nota 2: ");
+float not2= notas.nextFloat();
 
-Scanner cuartaNota = new Scanner(System.in);
-System.out.println("Ingrese nota 4: ");
-float not4= cuartaNota.nextFloat();
+System.out.print("Ingrese nota 3: ");
+float not3= notas.nextFloat();
 
-primeraNota.close();
-segundaNota.close();
-terceraNota.close();
-cuartaNota.close();
+System.out.print("Ingrese nota 4: ");
+float not4= notas.nextFloat();
 
-promedioNotas(not1,not2,not3,not4);
+notas.close();
+
+promedioNotas(not1,not2,not3,not4);*/
 
 
 //----------------------------Ejercicio 6 -------------------------------------
 //Escriba un programa que permita conocer el sueldo semanal de un trabajador en base a las horas que trabaja y el valor por hora ($H/H) que recibe.
 
+/*Scanner datos = new Scanner(System.in);
+System.out.print("Ingrese valor de Hora: ");
+int horas= datos.nextInt();
+System.out.print("Ingrese cantidad de horas: ");
+int valor= datos.nextInt();
 
+datos.close();
+
+sueldoSemanal(horas, valor);*/
+
+//----------------------------Ejercicio 7 -------------------------------------
+//. Escriba un programa que permita determinar cuánto cobra un pintor por pintar una cantidad dada de metros cuadrados, a partir del costo ingresado por el usuario
+
+/*Scanner datos = new Scanner(System.in);
+System.out.print("Ingresa numero de m2: ");
+int metrosPintura= datos.nextInt();
+System.out.print("Ingresa el monto por m2: ");
+int montoCobrado= datos.nextInt();
+
+datos.close();
+valorPintor(metrosPintura,montoCobrado);*/
+
+//----------------------------Ejercicio 8 -------------------------------------
+//Escriba un programa que permita determinar cuánto cobra un pintor por pintar una cantidad dada de metros cuadrados, a partir del costo ingresado por el usuario.
+
+/*Scanner teclado = new Scanner(System.in);
+System.out.print("Ingresa los minutos ultilizados: ");
+float minutos= teclado.nextFloat();
+System.out.print("Ingresa los segundos ultilizados: ");
+float segundos= teclado.nextFloat();
+System.out.print("Ingresa valor del minuto: ");
+float valorMinuto= teclado.nextInt();
+
+teclado.close();
+
+valorLLamada( minutos, segundos, valorMinuto);*/
+
+
+//----------------------------Ejercicio 9 -------------------------------------
+//Escriba un algoritmo que permita para determinar cuánto pagará finalmente una persona por un artículo, considerando que tiene un descuento de 20%, y debe pagar 15% de IVA
 
 
 
